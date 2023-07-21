df9d23553212a07f9a7ab10aebbafe7ac4d37d1d
devviettien <devbuinhathoang@gmail.com>
2023-07-21 19:25:06 +0700

    Initial Commit

---
+++ TinderClone/TinderCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderCloneApp.swift
+//  TinderClone
+//
+//  Created by Nhật Hoàng on 21/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct TinderCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
