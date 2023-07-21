b30548559b2d76b098812bf9d6b6f104adfad694
devviettien <devbuinhathoang@gmail.com>
2023-07-21 21:02:24 +0700

    success

---
+++ TinderClone/Trip.swift
@@ -0,0 +1,23 @@
+//
+//  Trip.swift
+//  TinderClone
+//
+//  Created by Nhật Hoàng (Việt Tiến) on 21/07/2023.
+//
+
+import Foundation
+
+struct Trip {
+    var destination: String
+    var image: String
+}
+
+
+var trips = [ Trip(destination: "Địch Lệ Nhiệt Ba", image: "dichlenhietba"),
+              Trip(destination: "Dương Tử", image: "duongtu"),
+              Trip(destination: "Tống Tổ Nhi", image: "tongtonhi"),
+              Trip(destination: "Bạch Lộc", image: "bachloc"),
+              Trip(destination: "Lý Thấm", image: "lytham"),
+              Trip(destination: "Triệu Lộ Tư", image: "trieulotu"),
+              Trip(destination: "Cúc Tịnh Y", image: "cuctinhy")
+            ]
